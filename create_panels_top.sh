4559581cd4098b98f5781528c4942bd08f1a0c84
David Pete <dervid9@gmail.com>
2023-10-27 00:04:01 +0200

    gerberek, bom

--- create_panels_top.sh
+++ create_panels_top.sh
@@ -1,6 +1,6 @@
 kikit panelize \
     --layout 'grid; rows: 5; cols: 3; space: 2mm' \
-    --tabs 'fixed; width: 5mm; vcount: 3; hcount: 2' \
+    --tabs 'annotation' \
     --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
     --post 'millradius: 1mm' \
     top.kicad_pcb panel_top.kicad_pcb
