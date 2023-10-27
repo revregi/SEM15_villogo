464da81942fad6a82b135d2c3d6ce2aaa4c2f076
David Pete <dervid9@gmail.com>
2023-10-27 14:08:02 +0200

    sent to jlc

--- create_panels_top.sh
+++ create_panels_top.sh
@@ -1,5 +1,5 @@
 kikit panelize \
-    --layout 'grid; rows: 5; cols: 3; space: 2mm' \
+    --layout 'grid; rows: 3; cols: 3; space: 2mm' \
     --tabs 'annotation' \
     --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
     --post 'millradius: 1mm' \
